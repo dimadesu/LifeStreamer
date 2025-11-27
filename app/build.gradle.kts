ee0205bdb2cf9182d22d94ab1f6268de16cc8d68
dimadesu <dimadesu@gmail.com>
2025-11-27 14:53:25 +1100

    Version bump to 1.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         compileSdk = 36
 
         versionCode = 1
-        versionName = "1.9.0"
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
