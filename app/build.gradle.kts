a65c69e0cb9fbf48d0f4aea1b80e289cbafe1039
dimadesu <dimadesu@gmail.com>
2026-01-17 21:38:39 +1100

    Update versionName to 1.19.2, versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         targetSdk = 35
         compileSdk = 36
 
-        versionCode = 2
-        versionName = "1.19.1"
+        versionCode = 3
+        versionName = "1.19.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
