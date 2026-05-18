291276a01652edd06641334db29303da0e3415b8
dimadesu <dimadesu@gmail.com>
2026-05-18 20:49:20 +1000

    Move streaming endpoints to the top of list in UI

--- app/src/main/java/com/dimadesu/lifestreamer/models/EndpointType.kt
+++ app/src/main/java/com/dimadesu/lifestreamer/models/EndpointType.kt
@@ -19,15 +19,15 @@ import androidx.annotation.StringRes
 import com.dimadesu.lifestreamer.R
 
 enum class EndpointType(val id: Int, @StringRes val labelResId: Int) {
+    SRT(2, R.string.to_srt),
+    SRTLA(8, R.string.to_srtla),
+    RTMP(3, R.string.to_rtmp),
     TS_FILE(0, R.string.to_ts_file),
     FLV_FILE(1, R.string.to_flv_file),
-    SRT(2, R.string.to_srt),
-    RTMP(3, R.string.to_rtmp),
     MP4_FILE(4, R.string.to_mp4_file),
     WEBM_FILE(5, R.string.to_webm_file),
     OGG_FILE(6, R.string.to_ogg_file),
-    THREEGP_FILE(7, R.string.to_3gp_file),
-    SRTLA(8, R.string.to_srtla);
+    THREEGP_FILE(7, R.string.to_3gp_file);
 
     companion object {
         fun fromId(id: Int): EndpointType = entries.first { it.id == id }
