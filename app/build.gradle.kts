ed079489521fdb4534022e52a98017483e199718
dimadesu <dimadesu@gmail.com>
2026-08-25 20:38:47 +1000

    Set versionCode = 36, versionName = "1.41.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         targetSdk = 36
         compileSdk = 36
 
-        versionCode = 35
-        versionName = "1.40.2"
+        versionCode = 36
+        versionName = "1.41.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
